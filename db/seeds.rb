665eb3cbebb4039afcae741d110600b4c8d29e42
Simon Granger <nothem6@gmail.com>
2021-08-11 17:38:11 +0200

    air

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+place1 = Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+place2 = Flat.create!(
+  name: 'uggly place',
+  address: '10 Clifton Gardens Lyon W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 72,
+  number_of_guests: 2
+)
+
+place3 = Flat.create!(
+  name: 'very uggly place',
+  address: '10 Clifton Gardens Paris W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 150,
+  number_of_guests: 1
+)
+
+place4 = Flat.create!(
+  name: 'Very very uggly place',
+  address: '10 Clifton Gardens Roanne W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 10,
+  number_of_guests: 5
+)
